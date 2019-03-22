f06d16266ea650203b01154a4aee3dcd211bf302
StarmanW <swkf3011@hotmail.com>
2019-03-23 01:02:03 +0800

    Added tab fragments, Google Firestore, Google Authentication, Registration page (Partially Complete) and DB Models

--- app/src/main/java/com/tarcrsd/letsgo/MainActivity.java
+++ app/src/main/java/com/tarcrsd/letsgo/MainActivity.java
@@ -1,13 +1,170 @@
 package com.tarcrsd.letsgo;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    // CONSTANTS
+    private static final int RC_SIGN_IN = 1;
 
+    // Firebase references
+    private FirebaseAuth mAuth;
+    private FirebaseFirestore db;
+
+    /**
+     * On Create method
+     *
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initialize FirebaseApp, Firestore and FirebaseAuth
+        FirebaseApp.initializeApp(getApplicationContext());
+        db = FirebaseFirestore.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuth.signOut();
+        // Initialize UI components
+        initUI();
+
+        // If user is not signed in
+        if (mAuth.getCurrentUser() == null) {
+            signInUser();
+        }
+    }
+
+    private void initUI() {
+        initTabFragments();
+    }
+
+    /**
+     * Initialize tab fragments
+     */
+    private void initTabFragments() {
+        /*
+         * Create an instance of the tab layout from the view.
+         * Set the text for each tab.
+         */
+        TabLayout tabLayout = findViewById(R.id.tabLayout);
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.tab_upcomingEvents));
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.tab_attending));
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.tab_profile));
+
+        // Set the tabs to fill the entire layout.
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        // Use PagerAdapter to manage page views in fragments.
+        // Each page is represented by its own fragment.
+        final ViewPager viewPager = findViewById(R.id.pager);
+        final PagerAdapter adapter = new PagerAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(adapter);
+
+        // Setting a listener for clicks.
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+            }
+        });
+    }
+
+    /**
+     * Method to display sign in form and perform sign in operation
+     */
+    private void signInUser() {
+        // Add google authentication providers
+        List<AuthUI.IdpConfig> providers = Arrays.asList(new AuthUI.IdpConfig.GoogleBuilder().build());
+
+        // Create and launch sign-in intent
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .setTheme(R.style.AppTheme)
+                        .build(),
+                RC_SIGN_IN);
+    }
+
+    /**
+     * Create menu option at top
+     *
+     * @param menu
+     * @return
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // MenuInflater class is used to instantiate menu XML files into Menu objects.
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_bar, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    /**
+     * Result returned after sign in activity
+     *
+     * @param requestCode
+     * @param resultCode
+     * @param data
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == RESULT_OK) {
+                // Successfully signed in
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                db.collection("users/").document(user.getUid()).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if (!task.getResult().exists()) {
+                            Intent registerNewUserIntend = new Intent(getApplicationContext(), RegisterActivity.class);
+                            startActivity(registerNewUserIntend);
+                        } else {
+
+                        }
+                    }
+                });
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                // ...
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/tarcrsd/letsgo/Models/EventAttendees.java
@@ -0,0 +1,4 @@
+package com.tarcrsd.letsgo.Models;
+
+public class EventAttendees {
+}
